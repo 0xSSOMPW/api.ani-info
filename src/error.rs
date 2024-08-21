092164f1f454425fe6d14183c250684b7fb48d82
falcon71181 <abhaythakur71181@gmail.com>
2024-08-21 07:41:32 +0530

    feat: implemented into response for custom error

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,37 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use std::fmt;
+
 #[derive(Debug)]
 pub enum CustomErrorENUM {
     DatabaseError(tokio_postgres::Error),
     NotFound,
 }
 
-impl std::fmt::Display for CustomErrorENUM {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+// Implement Display for CustomErrorENUM
+impl fmt::Display for CustomErrorENUM {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             CustomErrorENUM::DatabaseError(err) => write!(f, "Database error: {}", err),
             CustomErrorENUM::NotFound => write!(f, "Anime not found"),
+        }
+    }
+}
+
+// Implement IntoResponse for CustomErrorENUM
+impl IntoResponse for CustomErrorENUM {
+    fn into_response(self) -> Response {
+        match self {
+            CustomErrorENUM::DatabaseError(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Database error: {}", err),
+            )
+                .into_response(),
+            CustomErrorENUM::NotFound => {
+                (StatusCode::NOT_FOUND, "Anime not found".to_string()).into_response()
+            }
         }
     }
 }
