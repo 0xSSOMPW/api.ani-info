01f90461a7700436fd1f5789cc02148c45ff5934
falcon71181 <abhaythakur71181@gmail.com>
2024-08-22 08:26:22 +0530

    feat: db connection and operations

---
+++ src/db.rs
@@ -0,0 +1,103 @@
+use crate::error::CustomErrorENUM;
+use crate::model::{Anime, AnimeID, Episode};
+use crate::query::Query;
+use tokio_postgres::{Client, NoTls};
+
+pub async fn establish_connection(database_url: String) -> Result<Client, CustomErrorENUM> {
+    let (client, connection) = tokio_postgres::connect(&database_url, NoTls)
+        .await
+        .map_err(CustomErrorENUM::DatabaseError)?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Connection error: {:?}", e);
+        }
+    });
+
+    Ok(client)
+}
+
+pub async fn fetch_anime_ids(client: &Client) -> Result<Vec<AnimeID>, CustomErrorENUM> {
+    let rows = client
+        .query(Query::AnimeId.sql(), &[])
+        .await
+        .map_err(CustomErrorENUM::DatabaseError)?;
+
+    let anime_ids: Vec<AnimeID> = rows
+        .iter()
+        .map(|row| AnimeID {
+            id: row.get("id"),
+            anime_name: row.get("anime_name"),
+        })
+        .collect();
+
+    Ok(anime_ids)
+}
+
+pub async fn fetch_anime_detail_by_id(
+    client: &Client,
+    anime_id: i32,
+) -> Result<Anime, CustomErrorENUM> {
+    let rows = client
+        .query(Query::AnimeById.sql(), &[&(anime_id)])
+        .await
+        .map_err(CustomErrorENUM::DatabaseError)?;
+
+    if rows.is_empty() {
+        Err(CustomErrorENUM::NotFound)
+    } else {
+        let row = &rows[0];
+        let anime = Anime {
+            id: row.get("id"),
+            title: row.get("title"),
+            description: row.get("description"),
+            mal_id: row.get("mal_id"),
+            al_id: row.get("al_id"),
+            japanese_title: row.get("japanese_title"),
+            synonyms: row.get("synonyms"),
+            image: row.get("image"),
+            category: row.get("category"),
+            rating: row.get("rating"),
+            quality: row.get("quality"),
+            duration: row.get("duration"),
+            premiered: row.get("premiered"),
+            aired: row.get("aired"),
+            status: row.get("status"),
+            mal_score: row.get("mal_score"),
+            studios: row.get("studios"),
+            producers: row.get("producers"),
+            genres: row.get("genres"),
+            sub_episodes: row.get("sub_episodes"),
+            dub_episodes: row.get("dub_episodes"),
+            total_episodes: row.get("total_episodes"),
+            sub_or_dub: row.get("sub_or_dub"),
+        };
+        Ok(anime)
+    }
+}
+
+pub async fn fetch_episodes_by_anime_id(
+    client: &Client,
+    anime_id: i32,
+) -> Result<Vec<Episode>, CustomErrorENUM> {
+    let rows = client
+        .query(Query::EpisodesByAnimeId.sql(), &[&(anime_id)])
+        .await
+        .map_err(CustomErrorENUM::DatabaseError)?;
+
+    if rows.is_empty() {
+        Err(CustomErrorENUM::NotFound)
+    } else {
+        let episodes: Vec<Episode> = rows
+            .iter()
+            .map(|row| Episode {
+                id: row.get("id"),
+                title: row.get("title"),
+                episode_no: row.get("episode_no"),
+                is_filler: row.get("is_filler"),
+                anime_id: row.get("anime_id"),
+            })
+            .collect();
+        Ok(episodes)
+    }
+}
